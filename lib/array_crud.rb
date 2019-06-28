a8c525f1a9885566029deff69c90f0efa35e881d
Alena Nikulina <alenanikulina0@gmail.com>
2019-06-28 15:37:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Array.new("cat", "dog", "parrot", "dragon")
+  Array.new("cat", "dog", "parrot", "dragon", "snake")
 end
 
 def add_element_to_end_of_array(array, element)
